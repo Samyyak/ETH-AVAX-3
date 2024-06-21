d311f8918938b78ddaed1ba18a59cd385f612e38
Samyyak <108656939+Samyyak@users.noreply.github.com>
2024-06-22 03:30:46 +0530

    Update RewardToken.sol

--- RewardToken.sol
+++ RewardToken.sol
@@ -6,19 +6,51 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardToken is ERC20, Ownable {
 
+    event ApprovalLog(address indexed owner, address indexed spender, uint256 value, string message);
+
+    struct Lock {
+        uint256 amount;
+        uint256 unlockTime;
+    }
+
+    mapping(address => Lock) private _locks;
+
     constructor() ERC20("RewardToken", "RTK") Ownable(msg.sender) {
-        _mint(msg.sender, 1000 * 10 ** decimals()); 
+        _mint(msg.sender, 1000); 
     }
 
     function AddReward(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-
     function UseReward(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    // Users can transfer tokens using the standard ERC20 transfer functionality
-}
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        emit ApprovalLog(msg.sender, spender, amount, "Approval has been granted.");
+        return super.approve(spender, amount);
+    }
 
+    function lockTokens(uint256 amount, uint256 time) public {
+        require(balanceOf(msg.sender) >= amount, "ERC20: insufficient balance");
+        require(_locks[msg.sender].amount == 0, "ERC20: tokens already locked");
+
+        _locks[msg.sender] = Lock(amount, block.timestamp + time);
+        _burn(msg.sender, amount);
+    }
+
+    function unlockTokens() public {
+        require(_locks[msg.sender].amount > 0, "ERC20: no tokens locked");
+        require(block.timestamp >= _locks[msg.sender].unlockTime, "ERC20: tokens are still locked");
+
+        uint256 amount = _locks[msg.sender].amount;
+        _locks[msg.sender].amount = 0;
+        _mint(msg.sender, amount);
+    }
+
+    function getLockDetails(address _address) public view returns (uint256 amount, uint256 unlockTime) {
+        Lock storage lock = _locks[_address];
+        return (lock.amount, lock.unlockTime);
+    }
+}
